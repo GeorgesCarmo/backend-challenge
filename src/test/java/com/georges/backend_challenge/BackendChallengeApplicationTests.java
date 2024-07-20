33e3a81c4b0c1be64200456787f6ca5aa89800bb
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-07-19 21:16:18 -0300

    include methods for tests (create todo)

--- src/test/java/com/georges/backend_challenge/BackendChallengeApplicationTests.java
+++ src/test/java/com/georges/backend_challenge/BackendChallengeApplicationTests.java
@@ -1,13 +1,42 @@
 package com.georges.backend_challenge;
 
+import com.georges.backend_challenge.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendChallengeApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo1", "desc1", false, "baixa");
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody()
+				.jsonPath("name").isEqualTo(todo.getName())
+				.jsonPath("description").isEqualTo(todo.getDescription())
+				.jsonPath("completed").isEqualTo(todo.getCompleted())
+				.jsonPath("priority").isEqualTo(todo.getPriority());
+
+	}
+
+	@Test
+	void testCreateTodoFailure() {
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(new Todo("","",false, "none"))
+				.exchange()
+				.expectStatus().isEqualTo(500);
 	}
 
 }
